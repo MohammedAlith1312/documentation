93498eab6d4ad0e95e05f943bcf38b6947ba8262
mohammedalith <mohammedalith713@gmail.com>
2026-02-23 15:26:30 +0530

    'front'

--- docs/TextBlock.js
+++ docs/TextBlock.js
@@ -7,8 +7,8 @@
 (function () {
     // Use absolute URL for the backend since it runs on a different port (3000) than the frontend (5000)
     const API_BASE = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-        ? 'http://localhost:3000/api/issues'
-        : '/api/issues';
+        ? 'https://documentation-34fr.onrender.com/api/issues'
+        : 'https://documentation-34fr.onrender.com/api/issues';
 
     // --- Configuration & State ---
     const state = {
